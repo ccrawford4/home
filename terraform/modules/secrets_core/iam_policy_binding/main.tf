333f224455052c2af8e179b00a33cb2cb6c8b323
ccrawford4 <calumcrawford9@gmail.com>
2025-10-04 17:54:29 -0700

    update bindings and remoteRefKey

--- terraform/modules/secrets_core/iam_policy_binding/main.tf
+++ terraform/modules/secrets_core/iam_policy_binding/main.tf
@@ -16,13 +16,13 @@ resource "google_secret_manager_secret_iam_binding" "k8s_sa_secret_binding" {
   project = var.project_id
   secret_id = google_secret_manager_secret.secret.secret_id
   role = "roles/secretmanager.secretAccessor"
-members = [ "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/subject/ns/${var.k8s_namespace}/sa/${var.k8s_service_account}" ]
+  members = [ "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/subject/ns/${var.k8s_namespace}/sa/${var.k8s_service_account}", "serviceAccount:${var.google_service_account_email}" ]
 }
 
-# Grant the GCP service account access to the secret
-resource "google_secret_manager_secret_iam_binding" "google_sa_secret_binding" {
-  project = var.project_id
-  secret_id = google_secret_manager_secret.secret.secret_id
-  role = "roles/secretmanager.secretAccessor"
-  members = [ "serviceAccount:${var.google_service_account_email}" ]
-}
+# # Grant the GCP service account access to the secret
+# resource "google_secret_manager_secret_iam_binding" "google_sa_secret_binding" {
+#   project = var.project_id
+#   secret_id = google_secret_manager_secret.secret.secret_id
+#   role = "roles/secretmanager.secretAccessor"
+#   members = [ "serviceAccount:${var.google_service_account_email}" ]
+# }
--- terraform/modules/secrets_core/service_account/main.tf
+++ terraform/modules/secrets_core/service_account/main.tf
@@ -3,8 +3,8 @@ resource "google_service_account" "service_account" {
   display_name = "${var.account_id} Service Account"
 }
 
-resource "google_service_account_iam_binding" "workload_identity_user" {
-  service_account_id = google_service_account.service_account.name
-  role               = "roles/iam.workloadIdentityUser"
-members = [ "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/subject/ns/${var.k8s_namespace}/sa/${var.k8s_service_account}" ]
-}
+# resource "google_service_account_iam_binding" "workload_identity_user" {
+#   service_account_id = google_service_account.service_account.name
+#   role               = "roles/iam.workloadIdentityUser"
+# members = [ "principal://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/subject/ns/${var.k8s_namespace}/sa/${var.k8s_service_account}" ]
+# }
