d1297961b7393d6c51556f0622ee60f960c4d7ed
ccrawford4 <calumcrawford9@gmail.com>
2025-10-18 17:59:43 -0700

    configmaps and secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,20 @@ module "hello-world-secrets" {
    google_service_account_id = "hello-world"
    workload_identity_pool_id = google_iam_workload_identity_pool.home_cluster_pool.workload_identity_pool_id
 }
+
+module "search-app-secrets" {
+   source = "./modules/secrets_core"
+   project_id = var.project_id
+   project_number = var.project_number
+   region = var.region
+
+   label = "search-app"
+   k8s_namespace = "search-app"
+   k8s_service_account = "search-app"
+   secrets = [
+      "search-app-db-username",
+      "search-app-db-password"
+   ]
+   google_service_account_id = "search-app-sa"
+   workload_identity_pool_id = google_iam_workload_identity_pool.home_cluster_pool.workload_identity_pool_id
+}
