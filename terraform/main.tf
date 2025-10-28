28739f294207577d7fc035c238fae83c48e1b9b1
ccrawford4 <calumcrawford9@gmail.com>
2025-10-27 21:10:40 -0700

    update key

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ module "search-app-secrets" {
    secrets = [
       "search-app-db-username",
       "search-app-db-password",
-      "serach-app-db-root-username",
+      "serach-app-db-root-password",
    ]
    google_service_account_id = "secrets-manager-sa"
    workload_identity_pool_id = google_iam_workload_identity_pool.home_cluster_pool.workload_identity_pool_id
