81a98c489b4e4b53422bfdfe79dcb2b69af90eca
ccrawford4 <calumcrawford9@gmail.com>
2025-10-31 19:37:26 -0700

    add

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,11 @@ module "search-app-secrets" {
       "search-app-db-password",
       "search-app-db-root-password",
       "search-app-redis-password",
+      "search-app-nextauth-secret",
+      "search-app-github-id",
+      "search-app-github-secret",
+      "search-app-google-id",
+      "search-app-google-secret",
    ]
    google_service_account_id = "secrets-manager-sa"
    workload_identity_pool_id = google_iam_workload_identity_pool.home_cluster_pool.workload_identity_pool_id
