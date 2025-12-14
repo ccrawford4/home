dcde0c7f8e4b832a23603bb9c709f86c3a83425a
ccrawford4 <calumcrawford9@gmail.com>
2025-12-13 22:10:51 -0800

    ℹ️  Using Ollama provider at http://localhost:11434    Make sure Ollama is running: ollama serve [CE-XXXXX] feat: update Cloudflare Zero Trust Tunnel resource import

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -12,8 +12,3 @@ resource "cloudflare_zero_trust_tunnel_cloudflared" "master_tunnel" {
     ]
   }
 }
-
-import {
-  id = "${var.cloudflare_account_id}/1e543ef6-911a-43a4-9bb2-c3157fd3a02a"
-  to = cloudflare_zero_trust_tunnel_cloudflared.master_tunnel
-}
