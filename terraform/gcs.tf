9e0e7cad4c3878374df47d51505512bebc94b5bd
Calum Crawford <123438979+ccrawford4@users.noreply.github.com>
2025-12-13 20:59:09 -0800

    Merge pull request #11 from ccrawford4/9-gcs-bucket-as-terraform-for-tfstate
    feat: add lifecycle rules to tf state bucket

---
+++ terraform/gcs.tf
@@ -0,0 +1,48 @@
+resource "google_storage_bucket" "terraform_state_bucket" {
+  name = "tf-state-home-prod"
+  location = var.region
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 0
+      days_since_custom_time = 0
+      days_since_noncurrent_time = 0
+      matches_prefix = []
+      matches_storage_class = []
+      matches_suffix = []
+      no_age = false
+      num_newer_versions = 2
+      send_age_if_zero = true
+      send_days_since_custom_time_if_zero = false
+      send_days_since_noncurrent_time_if_zero = false
+      send_num_newer_versions_if_zero = false
+      with_state = "ARCHIVED"
+    }
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 0
+      days_since_custom_time = 0
+      days_since_noncurrent_time = 7
+      matches_prefix = []
+      matches_storage_class = []
+      matches_suffix = []
+      no_age = false
+      num_newer_versions = 0
+      send_age_if_zero = true
+      send_days_since_custom_time_if_zero = false
+      send_days_since_noncurrent_time_if_zero = false
+      send_num_newer_versions_if_zero = false
+      with_state = "ANY"
+    }
+  }
+}
