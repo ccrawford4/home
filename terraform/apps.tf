3494707667a9c0cfc9955135b5e0f1cd583936ed
ccrawford4 <calumcrawford9@gmail.com>
2026-07-02 21:38:14 -0700

    sec: protect argocd behind zero trust proxy

--- terraform/apps.tf
+++ terraform/apps.tf
@@ -36,3 +36,28 @@ resource "cloudflare_zero_trust_access_application" "home_master_k8s_api" {
   http_only_cookie_attribute = true
   options_preflight_bypass   = false
 }
+
+
+resource "cloudflare_zero_trust_access_application" "home_master_argocd" {
+  account_id                = var.cloudflare_account_id
+  name                      = "home-master-argocd"
+  type                      = "self_hosted"
+  allowed_idps              = []
+  auto_redirect_to_identity = false
+  session_duration          = "24h"
+  domain                    = "argocd.calum.sh"
+
+  destinations = [{
+    type = "public"
+    uri  = "argocd.calum.sh"
+  }]
+
+  policies = [{
+    id = cloudflare_zero_trust_access_policy.home_master_k8s_api_admin.id
+  }]
+
+  app_launcher_visible       = true
+  enable_binding_cookie      = false
+  http_only_cookie_attribute = true
+  options_preflight_bypass   = false
+}
